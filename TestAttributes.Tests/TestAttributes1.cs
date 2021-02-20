556d680599e9a4cdf29dea80c3cefeee4857ffd1
Ali <aliyildizoz909@gmail.com>
2021-02-20 14:18:48 +0300

    Test Attributes

---
+++ TestAttributes.Tests/TestAttributes1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestAttributes.Tests
+{
+    [TestClass]
+    public class TestAttributes1
+    {
+        [Owner("Ali")]
+        [TestMethod]
+        [TestCategory("Tester")]
+        [Priority(2)]
+        [TestProperty("Updated","Salih")]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1,1);
+        }
+        [TestCategory("Tester")]
+        [Owner("Ali")]
+        [Priority(4)]
+        [TestProperty("Updated","Salih")]
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestCategory("Developper")]
+        [TestProperty("Updated","Ali")]
+        [TestProperty("Updated2","Salih")] 
+        [Owner("Salih")]
+        [Priority(5)]
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestProperty("Updated","Engin")]
+        [TestCategory("Developper")]
+        [Owner("Salih")]
+        [Priority(3)]
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestProperty("Updated","Engin")]
+        [TestCategory("Developper")]
+        [Priority(1)]
+        [Owner("Salih")]
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ TestAttributes.Tests/TestAttributes2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAttributes.Tests
+{
+    [TestClass]
+    public class TestAttributes2
+    {
+        [TestMethod,Ignore]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod,Timeout(1000)]
+        public void TestMethod2()
+        {
+            //System.Threading.Thread.Sleep(5000);
+            Assert.AreEqual(1, 1);
+        }
+       
+        [TestMethod,Timeout(TestTimeout.Infinite)]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod, Ignore]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        
+        [TestMethod,Description("this is a method that do a sqrt test")]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
