3499ef0aa9fa716fbef6d9ced12d293dbaf0133c
Ali <aliyildizoz909@gmail.com>
2021-02-20 12:25:55 +0300

    Added Test Context Demo

---
+++ TestContextDemo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestContextDemo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestContextDemo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49aedf1c-5134-44ac-bdc7-eb81120902ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestContextDemo.Tests/TestContextTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestContextDemo.Tests
+{
+    [TestClass]
+    public class TestContextTests
+    {
+        //set at runtime
+        public TestContext TestContext { get; set; }
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestContext.WriteLine("--TestInitialize--\n");
+            TestContext.WriteLine("Test name: {0}", TestContext.TestName);
+            TestContext.WriteLine("Test state: {0}\n\n", TestContext.CurrentTestOutcome);
+            TestContext.Properties["info"] = "this is a extra information";
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestContext.WriteLine("--TestCleanup--\n");
+            TestContext.WriteLine("Test name: {0}", TestContext.TestName);
+            TestContext.WriteLine("Test state: {0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test info: {0}", TestContext.Properties["info"]);
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("--TestMethod1--\n");
+            TestContext.WriteLine("Test name: {0}", TestContext.TestName);
+            TestContext.WriteLine("Test state: {0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test class: {0}", TestContext.FullyQualifiedTestClassName); 
+            TestContext.WriteLine("Test info: {0}\n\n", TestContext.Properties["info"]);
+
+            Assert.AreEqual(1,1);
+        }
+    }
+}
